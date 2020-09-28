53da2bfad6960a7b1e8a7f5fa42a8eb3b4b3803e
lorenzo-damico <loredam@hotmail.it>
2020-09-28 15:38:31 +0200

    Setup

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// L'esercizio di oggi è quello di creare, come fatto in aula, una todo
+// list sulla quale sarà possibile svolgere le operazioni di CRUD, usando
+// qusta Api: http://157.230.17.132:3016/todos
+
+$(document).ready(function () {
+
+
+
+
+
+
+
+});
